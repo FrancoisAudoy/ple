11500c16e931e87b764c5e9c5747f1dfb4a37a70
Antonin Rebufat <arebufat@etu.u-bordeaux.fr>
2019-01-15 15:50:30 +0100

    mise a jour des couleur

--- projet/src/main/java/bigdata/StringUtils.java
+++ projet/src/main/java/bigdata/StringUtils.java
@@ -6,25 +6,26 @@ public final class StringUtils {
 
 
 	/*private static final Color NEGHIGHMAX = new Color(0x72b9e3);
-	private static final Color NEGHIGHMIN= new Color(0x0000e3);
+	private static final Color NEGHIGHMIN= new Color(0x000003);
 	private static final Color LOWHIGHMIN = new Color(0x90c46a);
 	private static final Color LOWHIGHMAX = new Color(0x00c400);
 	private static final Color MIDDLEHIGHMIN=new Color(0xd5b27f);
 	private static final Color MIDDLEHIGHMAX=new Color(0xb46b00);
 	private static final Color HIGHHIGHMIN = new Color(0xf4e3c3);
 	private static final Color HIGHHIGHMAX = new Color(0xeaeaea);*/
-	private static final int BARLOWMIDDLE=1000;
+	private static final int BARLOWMIDDLE=200;
 	private static final int BARMIDDLEHIGH=4000;
 	private static final int BARTOOHIGH=7000;
 	private static final int NEGHIGH=240;
-	private static final int LOWHIGH=120;
-	private static final int MIDDLEHIGH=60;
+	private static final int LOWHIGH=100;
+	private static final int MIDDLEHIGH=50;
 	private static final int HIGH=30;
 	private static final int TOOHIGH=0;
-	private static final float SATMAX=1.f;
-	private static final float SATMIN=.35f;
-	private static final float BRIMIN=.15f;
-	private static final float BRIMAX=.5f;
+	private static final float SATMAXNORMAL =1.f;
+	private static final float SATMINNORMAL =.6f;
+	private static final float BRIMINNORMAL =.3f;
+	private static final float BRIMAXNORMAL =.5f;
+	private static final float BRIMINHIGH=0.1f;
 
 
 	private static int [] colors; 
@@ -45,18 +46,21 @@ public final class StringUtils {
 		String [] lonLatparsed = new String[2];
 		lonLatparsed[0]=path.substring(1, 3);
 		lonLatparsed[1]=path.substring(4,7);//path.split("SsNnWwEe");
-		if (lonLatparsed.length == 2)
+		if (lonLatparsed.length == 2) {
 			return lonLatparsed;
-		else 
+		}else {
 			return null;
+		}
 	}
 	
 	public static void generatePalette(int min, int max) {
-		if(min < 0)
+		if(min < 0){
 			zeroAlt = Math.abs(min) + 1;
-		else 
+		}else {
 			zeroAlt = 0;
+		}
 		int size = max + Math.abs(min);
+
 		colors = new int [size];
 		for(int i = 0; i < size ; ++i) {
 			if(i < zeroAlt) {
@@ -75,37 +79,38 @@ public final class StringUtils {
 	}
 
 	private static int augCouleur(int color, int nb, int ite) {
-		float all=(SATMAX-SATMIN)+(BRIMAX-BRIMIN);
+		float all=(SATMAXNORMAL - SATMINNORMAL)+(BRIMAXNORMAL - BRIMINNORMAL);
 		float ecart=all/nb;
 		float sat=0.f;
 		float bri=0.f;
+		float limit=100*(BRIMAXNORMAL-BRIMINNORMAL)/all;
 		if(color==NEGHIGH){
-			sat=SATMAX;
-			bri=BRIMIN;
-			if(ite<(nb/10)*3.5){
-				bri+=ite*ecart;
-			}else{
-				bri=BRIMAX;
-				sat-=ite*ecart;
-			}
+			sat= SATMAXNORMAL;
+			bri= BRIMINNORMAL;
+			ecart=(BRIMAXNORMAL-BRIMINNORMAL)/nb;
+			bri+=ite*ecart;
 		}else if(color==TOOHIGH) {
 			sat=0.f;
 			ecart=0.6f/nb;
-			bri=BRIMAX-ite*ecart;
+			bri= BRIMAXNORMAL-ite*ecart;
+		}else if(color==HIGH) {
+			sat=SATMAXNORMAL;
+			ecart=(BRIMAXNORMAL-BRIMINHIGH)/nb;
+			bri=BRIMAXNORMAL-ite*ecart;
 		}else{
-			sat=SATMIN;
-			bri=BRIMAX;
-			if(ite<(nb/10)*5.5){
+			sat= SATMINNORMAL;
+			bri= BRIMAXNORMAL;
+			if(ite*ecart+sat<SATMAXNORMAL){
 				sat+=ite*ecart;
-			}else{
-				sat=SATMAX;
-				bri-=ite*ecart;
+			}else {
+				sat = SATMAXNORMAL;
+				bri -= ite*ecart;
 			}
 		}
 		return Color.getHSBColor(color/360f, sat, bri).getRGB();
 	}
 
-	public static int getRGBForThisHigh(int high) {
-		return (high<0)?colors[Math.abs(high)]:colors[high + zeroAlt];
+	public static int getRGBForThisHigh(short high) {
+		return (high<0)?colors[Math.abs(high)]:colors[high-1 + zeroAlt-1];
 	}
 }

ab95ac708ab30859560f19a4edbaaab8ac80856f
Antonin Rebufat <arebufat@etu.u-bordeaux.fr>
2019-01-25 15:27:21 +0100

    back en JEE

--- back/src/ServerTile.js
+++ back/src/ServerTile.js
@@ -4,13 +4,14 @@ const hbase=require('hbase');
 const pngjs=require('pngjs');
 const PNG = require('pngjs').PNG;
 
-let client=hbase({host:'0.0.0.0', port:8080});
+let client=hbase({host:'0.0.0.0', port:3001});
 
 router.get("/:x/:y/:z", async function(req, res){
-  let row=new hbase.Row(client, 'TilesAF', 'X17811118Y23232576');
+  let row=new hbase.Row(client, 'TilesAF', 'X0Y1006021Z1');
   val =row.get("Tile:img", (error, value) => {
     if(!error){
-      let png=new PNG({ filterType: 4 }).parse( value[0].$, function(error, data)
+      console.log(value);
+      let png=new PNG().parse( value[0].$, function(error, data)
       {
         console.log(error, data)
       });

10cffb90ce8ee32f3297f678787908c56048fe6b
Antonin Rebufat <arebufat@etu.u-bordeaux.fr>
2019-01-28 15:46:30 +0100

    mise en place du serveur et du front

--- front/main.js
+++ front/main.js
@@ -1,4 +1,5 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-L.tileLayer('http://young:3000/tiles/{x}/{y}/{z}', {
-    maxZoom: 1,
+var mymap = L.map('mapid').setView([0.00, 0.00], 6);
+L.tileLayer('http://young:8182/blackple/tiles/{z}/{x}/{y}', {
+    maxZoom: 6,
+    tileSize: 1201
 }).addTo(mymap);
